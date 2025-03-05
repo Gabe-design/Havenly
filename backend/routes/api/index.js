b826d422a56e34d6fd5147c88ea2cfd18f33c1c5
Gabe-design <gabrielaross8@gmail.com>
2025-03-05 01:44:27 -0800

    Add Users table, authentication middleware, and token handling

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,30 @@
 // api routes
 const router = require('express').Router();
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+router.use(restoreUser);
 
 // test route to verify api router is working
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
 });
+
+// Test setting a token cookie
+router.get('/set-token-cookie', async (_req, res) => {
+    const user = await User.findOne({ where: { username: 'Demo-lition' } });
+    setTokenCookie(res, user);
+    return res.json({ user });
+  });
+  
+  // Test restoring a user
+  router.get('/restore-user', (req, res) => {
+    return res.json(req.user);
+  });
+  
+  // Test requiring authentication
+  router.get('/require-auth', requireAuth, (req, res) => {
+    return res.json(req.user);
+  });
 
 module.exports = router;
