fb8cd0a98abda1b322cd2565237940b556e008d5
Gabe-design <gabrielaross8@gmail.com>
2025-03-11 17:53:33 -0700

    psql-setup-script

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,17 +1,9 @@
 const { sequelize } = require('./db/models');
 
-const setup = async () => {
-  try {
-    await sequelize.showAllSchemas({ logging: false });
-    if (process.env.NODE_ENV === "production") {
-      const schema = process.env.SCHEMA;
-      await sequelize.query(`CREATE SCHEMA IF NOT EXISTS ${schema};`);
-    }
-    console.log("Database setup complete!");
-  } catch (error) {
-    console.error("Database setup failed!", error);
-  }
-  process.exit();
-};
+// particular part of the database that will be use for this project
 
-setup();
+sequelize.showAllSchemas({ logging: false }).then(async (data) => {
+  if (!data.includes(process.env.SCHEMA)) {
+    await sequelize.createSchema(process.env.SCHEMA);
+  }
+});
