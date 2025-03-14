3d57e6ed5217f75ca58b6e4754f28c7c4c009d31
Gabe-design <gabrielaross8@gmail.com>
2025-03-14 15:00:07 -0700

    typo error

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+// Import API routes
+const apiRouter = require('./api');
+router.use('/api', apiRouter); // Only use API router
+
+
 // CSRF Route
 router.get("/api/csrf/restore", (req, res) => {
   res.cookie("XSRF-TOKEN", req.csrfToken());
   res.status(200).json({ "XSRF-Token": req.csrfToken() });
 });
 
-// Import API routes
-const apiRouter = require('./api');
-router.use('/api', apiRouter); // Only use API router
 
 // Test route
 router.get('/test', (req, res) => {
